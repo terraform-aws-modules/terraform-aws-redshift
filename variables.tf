6decd1c1917c3f638df2c8b9daff4f40255cd8e3
Woodland Hunter <woodland.hunter@moz.com>
2020-03-20 14:27:14 -0700

    snapshot id must be string not bool

--- variables.tf
+++ variables.tf
@@ -89,8 +89,8 @@ variable "vpc_security_group_ids" {
 
 variable "final_snapshot_identifier" {
   description = "(Optional) The identifier of the final snapshot that is to be created immediately before deleting the cluster. If this parameter is provided, 'skip_final_snapshot' must be false."
-  type        = bool
-  default     = false
+  type        = string
+  default     = ""
 }
 
 variable "skip_final_snapshot" {
